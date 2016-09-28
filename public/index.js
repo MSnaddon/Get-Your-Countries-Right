80ba873be41404e3fd090b3cdaf91e8eb9ee92d5
Michael Snaddon <msnaddon0991@gmail.com>
2016-09-28 15:01:26 +0100

    randomied quiz

--- public/index.js
+++ public/index.js
@@ -10,32 +10,47 @@ var processData = function(){
   var jsonString = this.responseText;
   var countries = JSON.parse(jsonString);
   makeQuiz(countries);
+  generateQuestion();
 }
 
 var quizCounter = 0;
-var quiz = [{countryName: "afghanistan", population: 7},{countryName: "edinburgh", population: 8}]
-var score = 0
+var quiz = [];
+var score = 0;
 
 var makeQuiz = function(countries){
-  for (var i = 0; i < 5; i++){
+  for (var i = 0; i < 11; i++){
+    index = Math.floor(Math.random()*countries.length)
+    
     var countryObject = {
-      countryName: countries[i].name,
-      population: countries[i].population
+      countryName: countries[index].name,
+      population: countries[index].population,
+      latLng: {
+        lat: countries[index].latlng[0],
+        lng: countries[index].latlng[1]
+      }
     };
+    console.log()
     quiz.push(countryObject);
   }
   console.log(quiz, " quiz so far")
 }
 
 var generateQuestion = function(){
-
-  
-
-  var questionText = "Is " + quiz[quizCounter].countryName +"'s " + quiz[quizCounter].population + " population greater or less than " + quiz[quizCounter+1].countryName;
   var question=document.querySelector('#question');
-  question.innerText=questionText;
-  quizCounter ++
   
+  if((quiz.length - 1) > quizCounter){
+
+    var questionText = "Is " + quiz[quizCounter].countryName +"'s " + quiz[quizCounter].population + " population greater or less than " + quiz[quizCounter+1].countryName + quiz[quizCounter+1].population;
+    question.innerText=questionText;
+    quizCounter ++
+  }
+  else{
+    // var endGame = "End of the game"
+    buttons = document.querySelectorAll("button")
+    buttons.forEach(function(button){button.disabled = true})
+    question.innerText = "End of the game. Your score was: " + score;
+    document.querySelector("#score").style.visibility = "hidden"
+  }
 }
 
 var moreClick = function(){
@@ -51,14 +66,21 @@ var lessClick = function(){
 }
 
 var compareAnswer = function(guess){
-  var answer = quiz[quizCounter-1].population < quiz[quizCounter].population ? "more" : "less";
+
+  var answer = quiz[quizCounter-1].population > quiz[quizCounter].population ? "more" : "less";
+
   if (guess === answer){
     score ++
   } else {
     console.log("you're bad and you should feel bad")
   }
+  updateScore();
 }
 
+var updateScore = function(){
+  var scoreDiv = document.querySelector("#score");
+  scoreDiv.innerText = "Score : " + score;
+}
 
 
 var app = function(){
@@ -71,7 +93,7 @@ var app = function(){
   var buttonLess = document.querySelector("#less");
   buttonLess.onclick = lessClick;
 
-  generateQuestion();
+  // makeQuiz(countries);
   // var question = document.querySelector
 }
 
